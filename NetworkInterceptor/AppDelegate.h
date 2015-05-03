7b26a9851638959b9339d72f6c3640d7b6363f26
Georgios Taskos <george@xplatsolutions.com>
2015-05-03 16:30:31 -0400

    Initial Commit

---
+++ NetworkInterceptor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkInterceptor
+//
+//  Created by Georgios Taskos on 5/3/15.
+//  Copyright (c) 2015 Xplat Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetworkInterceptor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkInterceptor
+//
+//  Created by Georgios Taskos on 5/3/15.
+//  Copyright (c) 2015 Xplat Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
